dfa14eaf7eedec3f362ea09c69c54b4be45b2530
inafrick <inaciopimentel7@gmail.com>
2023-11-13 13:56:25 -0300

    minha parte do exercicio avaliativo 3 concluida

--- Trabalho Final POO/src/JanelaEventos.java
+++ Trabalho Final POO/src/JanelaEventos.java
@@ -6,25 +6,24 @@ import java.util.Collections;
 
 public class JanelaEventos extends JFrame implements ActionListener {
     private JTextField codinome, quantidade, latitude, longitude;
-    private JButton botao, fechar;
-    private JTextArea area, areaEquipes;
+    private JButton botao, fechar, limpar, crescente;
+    private JTextArea area;
     private ArrayList<Equipe> equipes;
 
     public JanelaEventos() {
         super();
         equipes = new ArrayList<>();
-
         codinome = new JTextField(20);
         quantidade = new JTextField(3);
         latitude = new JTextField(20);
         longitude = new JTextField(20);
         botao = new JButton("OK");
         fechar = new JButton("Fechar");
+        limpar = new JButton("Limpar");
+        crescente = new JButton("Ordenar Crescente");
         area = new JTextArea();
-        areaEquipes = new JTextArea();
 
         area.setEditable(false);
-        areaEquipes.setEditable(false);
 
         JPanel container = new JPanel();
         BoxLayout layout = new BoxLayout(container, BoxLayout.PAGE_AXIS);
@@ -39,11 +38,14 @@ public class JanelaEventos extends JFrame implements ActionListener {
         container.add(new JLabel("Longitude: "));
         container.add(longitude);
         container.add(area);
-        container.add(areaEquipes);
         container.add(botao);
+        container.add(limpar);
+        container.add(crescente);
         container.add(fechar);
 
         botao.addActionListener(this);
+        limpar.addActionListener(this);
+        crescente.addActionListener(this);
         fechar.addActionListener(this);
 
         this.add(container);
@@ -69,19 +71,35 @@ public class JanelaEventos extends JFrame implements ActionListener {
                 quantidade.setText("");
                 latitude.setText("");
                 longitude.setText("");
-
-                Collections.sort(equipes);
-
-                areaEquipes.setText("");
-                for (Equipe eq : equipes) {
-                    areaEquipes.append(eq.toString());
-                }
+                area.append("\n\nDados da equipe cadastrada: " + equipe.getCodinome() + " - "
+                    + equipe.getQuantidade() + " - " + equipe.getLatitude() + " - " +
+                    equipe.getLongitude());
             }
             catch (NumberFormatException exception){
                 area.setText("Erro! Certifique-se de inserir números válidos para quantidade, latitude e longitude.");
             }
             catch (Exception exception){
                 area.setText(exception.getMessage());
+            }
+        }
+        else if (e.getSource() == limpar){
+            codinome.setText("");
+            quantidade.setText("");
+            latitude.setText("");
+            longitude.setText("");
+            area.setText("");
+        }
+        else if(e.getSource() == crescente){
+            if (equipes.isEmpty()){
+                area.setText("Nenhuma equipe cadastrada.");
+                return;
+            }
+
+            Collections.sort(equipes);
+
+            area.setText("Dados das equipes:\n\n");
+            for (Equipe eq : equipes) {
+                area.append(eq.toString());
             }
         }
         else if(e.getSource() == fechar) {
